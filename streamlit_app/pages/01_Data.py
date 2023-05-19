a2b37e3e0b6c3d2a667af7cfd07a18547d655eb5
Mathieu Beauvois <126469342+Ringingmathieu@users.noreply.github.com>
2023-05-19 14:37:05 +0200

    Update 01_Data.py

--- streamlit_app/pages/01_Data.py
+++ streamlit_app/pages/01_Data.py
@@ -4,7 +4,7 @@ import streamlit as st
 from streamlit_extras.dataframe_explorer import dataframe_explorer
 
 # import the dataset 
-video_game = pd.read_csv('Data/Video_Games.csv')
+video_game = pd.read_csv('/app/video_game_data_analysis/streamlit_app/Data/Video_Games.csv')
 
 # Create a header for the page
 st.title('Data exploration')
