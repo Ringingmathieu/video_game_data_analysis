7da1c4b5c49547dcd5e6562b27dafbec351c6ebb
Mathieu Beauvois <126469342+Ringingmathieu@users.noreply.github.com>
2023-05-19 13:27:26 +0200

    Update Analysis.py

--- streamlit_app/Analysis.py
+++ streamlit_app/Analysis.py
@@ -5,7 +5,7 @@ import plotly.subplots as sp
 import streamlit as st
 
 # import the dataset 
-video_game = pd.read_csv('Data/Video_Games.csv')
+video_game = pd.read_csv('Data/Video_Games.csv') //relative path
 
 ## clean the dataset for analysis
 
