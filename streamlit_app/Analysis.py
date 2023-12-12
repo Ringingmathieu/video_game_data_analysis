9040f99c4c859a29491902132726e2bbd9a51168
Mathieu Beauvois <126469342+Ringingmathieu@users.noreply.github.com>
2023-12-12 11:10:08 +0100

    Update Analysis.py

--- streamlit_app/Analysis.py
+++ streamlit_app/Analysis.py
@@ -148,8 +148,6 @@ the sales has been dropping every year.
 # count the sum of sales grouped by years
 sales_year = video_game.groupby('Year_of_Release').sum().reset_index()
 
-st.dataframe(sales_year)
-
 # create lineplot of sales by year and region
 fig_4 = px.line(sales_year, x='Year_of_Release', y=sales_year.columns,
             title='Sales by Year')
